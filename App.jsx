3506cf1902a108c732c918fc289fdc1f102eca53
MehmetCan Özoğlu <mehmetcanozoglu1@gmail.com>
2023-11-28 20:59:31 +0300

    React_Image_Gallery
    React_Image_Gallery

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Gallery from './Gallery'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+    <h1 className='mainH1'> İmage Gallery</h1>
+      <Gallery/>h1
+   
+    </>
+  )
+}
+
+export default App
